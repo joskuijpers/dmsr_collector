d576bda473774d8afd6aa3665f22553b65e529eb
Jos Kuijpers <jkuijpers@jarvix.nl>
2021-12-28 13:36:39 +0100

    Rough prototype of adding data to postgres table.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::time::Duration;
+use postgres::{Client, NoTls};
 use crate::parser::Parser;
 use crate::port::PortBuilder;
 use crate::reader::FrameReader;
@@ -10,11 +11,24 @@ mod reader;
 
 fn main() {
     // Open port
-    let port = PortBuilder::from_device("/dev/ttyUSB0");
+    // let port = PortBuilder::from_device("/dev/ttyUSB0");
 
     // Fake port
-    // let port = PortBuilder::from_data(include_str!("../../tests/testdata").as_bytes());
+    let port = PortBuilder::from_data(include_str!("../../tests/testdata").as_bytes());
     let mut frame_reader = FrameReader::new(port);
+
+    let mut client = Client::connect("host=localhost user=postgres", NoTls).unwrap();
+
+    client.batch_execute("
+        CREATE TABLE IF NOT EXISTS dsmr_raw (
+            id                  SERIAL PRIMARY KEY,
+            time                TEXT NOT NULL,
+            delivering          DOUBLE PRECISION NOT NULL,
+            delivered_t1        DOUBLE PRECISION NOT NULL,
+            delivered_t2        DOUBLE PRECISION NOT NULL,
+            gas_delivered       DOUBLE PRECISION NOT NULL
+        )
+    ").unwrap();
 
     loop {
         if let Some(raw_frame) = frame_reader.read_next_byte() {
@@ -27,6 +41,17 @@ fn main() {
                      data_frame.data.electricity_delivered_t2,
                      data_frame.data.gas_delivered,
             );
+
+            client.execute(
+                "INSERT INTO dsmr_raw (time, delivering, delivered_t1, delivered_t2, gas_delivered) VALUES ($1, $2, $3, $4, $5)",
+                &[
+                    &data_frame.time.to_string(),
+                    &data_frame.data.electricity_delivering,
+                    &data_frame.data.electricity_delivered_t1,
+                    &data_frame.data.electricity_delivered_t2,
+                    &data_frame.data.gas_delivered
+                ],
+            ).unwrap();
 
             // DSMR only does frames every 1 second.
             std::thread::sleep(Duration::from_millis(250));
