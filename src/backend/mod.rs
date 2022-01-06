fa8e29750d8965aa2ba0e972a0e7f8c229f65616
Jos Kuijpers <jkuijpers@jarvix.nl>
2022-01-06 11:03:10 +0100

    Fix building when database feature is disabled.

--- src/backend/mod.rs
+++ src/backend/mod.rs
@@ -2,9 +2,13 @@ use std::fmt::Error;
 use crate::DataFrame;
 
 mod api;
+
+#[cfg(feature = "database")]
 mod database;
 
 pub use api::*;
+
+#[cfg(feature = "database")]
 pub use database::*;
 
 pub trait Backend {
